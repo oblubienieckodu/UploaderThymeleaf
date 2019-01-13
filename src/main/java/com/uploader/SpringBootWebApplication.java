e2651713f579254320753cdc0bf8ee200ee17c64
vitaliyRysich <vitaliy.rysich@gmail.com>
2019-01-13 19:22:47 +0100

    test commit

--- src/main/java/com/uploader/SpringBootWebApplication.java
+++ src/main/java/com/uploader/SpringBootWebApplication.java
@@ -21,7 +21,7 @@ public class SpringBootWebApplication {
     public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
 
         TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-
+//test
         tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
             if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
                 //-1 means unlimited
